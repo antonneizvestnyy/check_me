7cdc2d8ff4ff45af52e88416a63feeba528e1af2
antonneizvestnyy <spectreawplanet@gmail.com>
2023-12-16 20:48:03 +0300

    add/del issue assignee

---
+++ For_Work/JIRA/REST_API/JIRA/issue_null_assignee.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+login="LOGIN:PASSWORD"
+
+curl -X PUT \
+      --url "<DOMAIN_NAME>/rest/api/2/issue/<ISSUE_KEY>/assignee" \
+      -u $login \
+      -H "Accept: application/json" \
+      -H "Content-Type: application/json" \
+      -d '{
+   "name": null
+}'
